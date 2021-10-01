a7c81e84e375ac4a3a06c3ffc43a82dc462d74b6
nmancus1 <nicholas.mancuso13@gmail.com>
2021-10-01 07:52:13 -0400

    added python script and relevant files

--- compare_media.py
+++ compare_media.py
@@ -1,13 +1,28 @@
-filepath = 'db-1-made-pretty.json'
-substring = "jpg"
+# filepath = 'db-1-made-pretty.json'
+matches = ["jpg", "png"]
+
+# cnt = 1
+
+# with open(filepath) as fp:
+#    line = fp.readline()
+#    while line:
+#         if any(x in line for x in matches):
+#             arr =  line.rsplit();
+#             filename = arr[1]
+#             a_filename = filename.replace('"', '').replace(',', '')
+#             print(a_filename);
+#             cnt +=1
+#         line = fp.readline()
+
+
+filepath = 'old_media_backup_list.txt'
 cnt = 1
+
 with open(filepath) as fp:
    line = fp.readline()
    while line:
-        if substring in line:
-            print(line)
-            # line.rsplit();
+        if any(x in line for x in matches):
+            a_filename = line.replace('/home/nick/CIVAM/CIVAM_BACKUP/civam-media-backup/media_10-22-2020/', '')
+            print(a_filename)
+            cnt += 1
         line = fp.readline()
-        cnt += 1
-
-print(f"number jpeg: {cnt}")
